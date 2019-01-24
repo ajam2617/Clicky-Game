b4ce7ad5c1ba6d3f48235ab67d525fd3ac18af39
Mayja Tom <maja.morales17@gmail.com>
2019-01-23 22:27:47 -0800

    Getting html to display.

---
+++ drag-click/src/components/GameContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function GameContainer() {
+    return (
+        <div className="container">
+
+            <h1>pics to click</h1>
+
+        </div>
+    )
+}
+
+export default GameContainer;
---
+++ drag-click/src/components/NavTabs.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavTabs () {
+    return(
+        <div className= "container">
+            <jumbotron>
+                <h1>Drag Clicky Game</h1>
+
+                <p>Only click the faces once, and don't F*** it up.</p> 
+
+            </jumbotron>
+        </div>
+    )
+}
+
+export default NavTabs;

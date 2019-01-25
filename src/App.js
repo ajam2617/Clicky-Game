2d8bececc3c766ebc3639e4e5dae40bf5c1d2932
Mayja Tom <maja.morales17@gmail.com>
2019-01-24 20:16:28 -0800

    adding images to json file. Ran test to get images. Failed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import NavTabs from "./components/NavTabs";
 import GameContainer from "./components/GameContainer";
+import cards from "./cards.json"
+import Wrapper from "./components/Wrapper/index";
+import Title from "./components/Title/index";
+import SpaceCard from './components/Cards';
 
-function App () {
-  return (
-      <div>
-        <NavTabs />
-        <GameContainer />
-      </div>
+class App extends Component {
+  state = {
+    cards
+  };
+  render() {
+
+  }
+  render() {
+    return (
+      <Wrapper>
+        <Title>Image Shuffle</Title>
+        {this.state.cards.map(cards => (
+          <SpaceCard
+            id={cards.id}
+            image={cards.image}
+          />
+        ))}
+      </Wrapper>
     );
+  }
 }
 
 export default App;

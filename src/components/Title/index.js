2dae00426b667bf31319d05514fbcf77791c5d4f
Mayja Tom <maja.morales17@gmail.com>
2019-01-26 20:43:32 -0800

    adding more styling and icons

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-const Title = (props) =>(
+const Title = () =>(
   <div className="banner">
-  <h1 className="text-center">Galaxy Memory Game!</h1>
+  <h1 className="text-center"><i className="fa fa-space-shuttle fa-2x fa-rotate-270"></i>
+
+Galaxy Memory Game!<i className="fa fa-space-shuttle fa-2x fa-rotate-270"></i>
+</h1>
   </div>
 )
 

c4f46e763208ea000f101ae1e4b76c0f401f48d5
Mayja Tom <maja.morales17@gmail.com>
2019-01-31 12:06:07 -0800

    Updates

---
+++ precache-manifest.f356192c598120ad882d17c5ef37d6cd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0dccb34ba7fa4ee742e6d83602752f2",
+    "url": "/Clicky-Game/static/media/planet4.c0dccb34.JPG"
+  },
+  {
+    "revision": "c6ee4c4d774d7f397f26",
+    "url": "/Clicky-Game/static/css/main.45409ffc.chunk.css"
+  },
+  {
+    "revision": "3afb3cb987fe3012718b",
+    "url": "/Clicky-Game/static/js/1.3afb3cb9.chunk.js"
+  },
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "7c6d7665e33641e250ef1f88310508bf",
+    "url": "/Clicky-Game/static/media/planet1.7c6d7665.JPG"
+  },
+  {
+    "revision": "983ddfebbcd9ceee3fcc014058bb62f4",
+    "url": "/Clicky-Game/static/media/planet2.983ddfeb.JPG"
+  },
+  {
+    "revision": "6ee35ab409518037b16083bb60a23ba7",
+    "url": "/Clicky-Game/static/media/planet3.6ee35ab4.JPG"
+  },
+  {
+    "revision": "c6ee4c4d774d7f397f26",
+    "url": "/Clicky-Game/static/js/main.c6ee4c4d.chunk.js"
+  },
+  {
+    "revision": "6b798fd6eada4fc3c05a18d79cfa0f3b",
+    "url": "/Clicky-Game/static/media/spaceship1.6b798fd6.JPG"
+  },
+  {
+    "revision": "9e7ae79c3736e241cf8b54d5fd99874d",
+    "url": "/Clicky-Game/static/media/spaceship2.9e7ae79c.JPG"
+  },
+  {
+    "revision": "1f41ee9b43560562500d6829ef43fa80",
+    "url": "/Clicky-Game/static/media/spaceship3.1f41ee9b.JPG"
+  },
+  {
+    "revision": "5535189da20bb9fdc522dcf7e2e853b1",
+    "url": "/Clicky-Game/static/media/spaceship4.5535189d.JPG"
+  },
+  {
+    "revision": "d0684142a50e435ca62d44693e4b670d",
+    "url": "/Clicky-Game/static/media/spaceship5.d0684142.JPG"
+  },
+  {
+    "revision": "0d4249eabe547dd1db0a5512f476a543",
+    "url": "/Clicky-Game/static/media/spaceship6.0d4249ea.JPG"
+  },
+  {
+    "revision": "5e1b103c45afdd6059dbaa776c3a6d39",
+    "url": "/Clicky-Game/index.html"
+  }
+];
